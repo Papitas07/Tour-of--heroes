886e6e633fa316924a8fbd2118e19c3609d091bb
Fernandez Emilie <emilie.fernandez1@hotmail.com>
2019-08-27 14:07:53 +0200

    end routes + begin http

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,11 @@
+import { InMemoryDataService } from 'angular-in-memory-web-api';
+import  { Hero } from './hero';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService {
+
+  constructor() { }
+}
